d9c86f7f802ee1ad3c9d972bb0816ba07a0c401d
rakeshminase <rakeshminase@example.com>
2023-04-04 10:50:15 +0530

    Done Assignments from Basic section

---
+++ Basics/A1.py
@@ -0,0 +1,13 @@
+var1 = 10
+var2 = 20
+var3 = var1 + var2
+print(var3)
+print(id(var3))
+dic1 = {1: "a", 2: "b"}
+dic2 = {3: "c"}
+
+dic3 = dic1 | dic2
+
+print(dic1)
+print(dic2)
+print(dic3)
---
+++ Basics/A2.py
@@ -0,0 +1,6 @@
+# * is used for unpacking list, set and tuple while
+# ** is used for unpacking dictionary
+
+li = [1, 2, 3, 4, 5, 6, 7]
+
+print(*li)

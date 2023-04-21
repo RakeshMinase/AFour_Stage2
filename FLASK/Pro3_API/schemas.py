d9712ec5f31b22a1b60a0e4399dc19aadbf2a368
rakeshminase <rakeshminase@example.com>
2023-04-21 18:02:28 +0530

    Added Flask api 3 project

---
+++ FLASK/Pro3_API/schemas.py
@@ -0,0 +1,26 @@
+from marshmallow import Schema, fields
+
+
+class PlainItemSchema(Schema):
+    id = fields.Str(dump_only=True)
+    name = fields.Str(required=True)
+    price = fields.Float(required=True)
+
+
+class PlainStoreSchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.Str()
+
+
+class StoreSchema(PlainStoreSchema):
+    items = fields.Nested(PlainItemSchema(), dump_only=True)
+
+
+class ItemSchema(PlainItemSchema):
+    store_id = fields.Int(required=True, load_only=True)
+    store = fields.Nested(PlainStoreSchema(), dump_only=True)
+
+
+class ItemUpdateSchema(Schema):
+    price = fields.Float()
+    name = fields.Str()
